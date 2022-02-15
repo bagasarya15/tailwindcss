54e1893a1731a2f4cfa18feb8e404bca4cc3e715
bagasarya15 <bagasarya150800@gmail.com>
2022-02-16 02:07:05 +0700

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        13: '3.25rem',
+        15: '3.75rem',
+        128: '32rem',
+        144: '36rem',
+      },
+    },
+  },
+};
